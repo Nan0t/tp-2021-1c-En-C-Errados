1a562ae346fb6e7be6d2dd22e20dd68e8e110b03
Hernan Solis <hsolis@est.frba.utn.edu.ar>
2021-04-30 15:38:31 -0300

    feat: estructuras comunes a los protocolos

---
+++ utils/include/utils/protocol/buffer.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_PROTOCOL_BUFFER_H
+#define UTILS_PROTOCOL_BUFFER_H
+
+#include "utils/typedefs.h"
+typedef struct {
+    uint32_t size;
+    void*    data;
+} buffer_t;
+
+#endif
---
+++ utils/include/utils/protocol/paquete.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_PROTOCOL_PAQUETE_H
+#define UTILS_PROTOCOL_PAQUETE_H
+
+#include "utils/protocol/buffer.h"
+
+typedef struct {
+    uint32_t  opCodeMsg; 
+    buffer_t* buffer;
+} paquete_t;
+
+#endif
