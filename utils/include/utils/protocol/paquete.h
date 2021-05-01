812194e430e52ae742b17233f3b4a40ce112f398
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-01 15:11:01 -0300

    [feat]: funciones de utilidad para el struct paquete

--- utils/include/utils/protocol/paquete.h
+++ utils/include/utils/protocol/paquete.h
@@ -9,4 +9,9 @@ typedef struct {
     u_buffer_t* buffer;
 } u_paquete_t;
 
+u_paquete_t* u_paquete_crear(u_opcode_e opcode, const u_buffer_t* buffer);
+void         u_paquete_delete(u_paquete_t* this);
+
+u_buffer_t* u_paquete_serializar(const u_paquete_t* this);
+
 #endif
---
+++ utils/src/protocol/paquete.c
@@ -0,0 +1,33 @@
+#include "utils/protocol/paquete.h"
+#include "utils/memory/allocator.h"
+
+u_paquete_t* u_paquete_crear(u_opcode_e opcode, const u_buffer_t* buffer)
+{
+    u_paquete_t* this = u_malloc(sizeof(u_paquete_t));
+
+    this->opCodeMsg = opcode;
+    this->buffer    = u_buffer_duplicate(buffer);
+
+    return this;
+}
+
+void u_paquete_delete(u_paquete_t* this)
+{
+    if(this != NULL)
+    {
+        if(this->buffer != NULL)
+            u_buffer_delete(this->buffer);
+
+        u_free(this);
+    }
+}
+
+u_buffer_t* u_paquete_serializar(const u_paquete_t* paquete)
+{
+    u_buffer_t* buffer = u_buffer_create();
+
+    u_buffer_write(buffer, &paquete->opCodeMsg, sizeof(uint32_t));
+    u_buffer_write(buffer, u_buffer_get_content(paquete->buffer), u_buffer_get_size(paquete->buffer));
+
+    return buffer;
+}
