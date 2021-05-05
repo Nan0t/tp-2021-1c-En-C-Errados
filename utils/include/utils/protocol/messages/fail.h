1998fe902da504493ce2c3efff3945606ce5af48
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-05 16:03:06 -0300

    [feat]: fail message

---
+++ utils/include/utils/protocol/messages/fail.h
@@ -0,0 +1,20 @@
+#ifndef UTILS_PROTOCOL_MESSAGE_FAIL_H
+#define UTILS_PROTOCOL_MESSAGE_FAIL_H
+
+#include "utils/protocol/paquete.h"
+
+typedef struct
+{
+    char* description;
+} u_msg_fail_t;
+
+u_msg_fail_t* u_msg_fail_crear(const char* format, ...) UTILS_PRINT_FORMAT(1, 2);
+
+u_paquete_t* u_msg_fail_serializar(const u_msg_fail_t* this);
+u_msg_fail_t* u_msg_fail_deserializar(const u_buffer_t* buffer);
+
+void u_msg_fail_eliminar(u_msg_fail_t* this);
+
+char* u_msg_fail_to_string(const u_msg_fail_t* this);
+
+#endif
