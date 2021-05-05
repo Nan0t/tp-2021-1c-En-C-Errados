83df24b745488386705ba57bc93f773c5c9150b0
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-05 16:16:04 -0300

    [fix]: no se estaba agregando el tamaño del buffer del paquete al serializar

--- utils/src/protocol/paquete.c
+++ utils/src/protocol/paquete.c
@@ -25,8 +25,10 @@ void u_paquete_delete(u_paquete_t* this)
 u_buffer_t* u_paquete_serializar(const u_paquete_t* paquete)
 {
     u_buffer_t* buffer = u_buffer_create();
+    uint32_t paquete_buffer_size = u_buffer_get_size(paquete->buffer);
 
     u_buffer_write(buffer, &paquete->opCodeMsg, sizeof(uint32_t));
+    u_buffer_write(buffer, &paquete_buffer_size, sizeof(uint32_t));
     u_buffer_write(buffer, u_buffer_get_content(paquete->buffer), u_buffer_get_size(paquete->buffer));
 
     return buffer;
