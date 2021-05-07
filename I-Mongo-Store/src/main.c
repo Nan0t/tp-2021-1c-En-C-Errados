94db08ac4b6848ea37eb25449f0a3e0d9189abef
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-07 11:38:25 -0300

    [feat]: Inicializacion del File System

--- I-Mongo-Store/src/main.c
+++ I-Mongo-Store/src/main.c
@@ -1,4 +1,9 @@
+#include "fs/file_system.h"
+#include "server/server.h"
+
 #include <utils/entry_point.h>
+
+#include <stdlib.h>
 
 app_attr_t get_app_setup(void)
 {
@@ -11,11 +16,48 @@ app_attr_t get_app_setup(void)
     };
 }
 
+private t_list* get_sabotage_positions(void);
+
 int entry_point(int argc, char** argv)
 {
     (void)argc; // --> para evitar el warning unused_parameter
     (void)argv; // --> para evitar el warning unused_parameter
 
-    U_LOG_INFO("Modulo I-Mongo-Store");
+    file_system_init(&(file_system_attr_t) {
+        .mount_point        = u_config_get_string_value("PUNTO_MONTAJE"),
+        .sabotage_positions = get_sabotage_positions(),
+        .sync_time          = u_config_get_int_value("TIEMPO_SINCRONIZACION")
+    });
+
+    fs_server_init(u_config_get_string_value("PUERTO"));
+
     return 0;
+}
+
+private t_list* get_sabotage_positions(void)
+{
+    char** positions           = u_config_get_array_value("POSICIONES_SABOTAJE");
+    t_list* sabotage_positions = list_create();
+
+    for(char** pos = positions; *pos != NULL;  pos ++)
+    {
+        u_pos_t* pos_struct = u_malloc(sizeof(u_pos_t));
+
+        char** pos_values = string_split(*pos, "|");
+
+        pos_struct->x = (uint32_t)atoi(pos_values[0]);
+        pos_struct->y = (uint32_t)atoi(pos_values[1]);
+
+        list_add(sabotage_positions, pos_struct);
+
+        u_free(pos_values[0]);
+        u_free(pos_values[1]);
+        u_free(pos_values);
+
+        u_free(*pos);
+    }
+
+    u_free(positions);
+
+    return sabotage_positions;
 }
