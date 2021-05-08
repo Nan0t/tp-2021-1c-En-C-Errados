d09040078b8667bee73c1565a3036e5b3c9a67a8
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-08 18:35:01 -0300

    [feat]: interfaz servidor de Mi-RAM HQ

---
+++ Mi-RAM-HQ/src/servidor/servidor.c
@@ -0,0 +1,6 @@
+#include "servidor.h"
+
+bool servidor_init(const char* port)
+{
+    
+}
---
+++ Mi-RAM-HQ/src/servidor/servidor.h
@@ -0,0 +1,8 @@
+#ifndef MEMORIA_SERVIDOR_H
+#define MEMORIA_SERVIDOR_H
+
+#include <utils/utils.h>
+
+bool servidor_init(const char* port);
+
+#endif
