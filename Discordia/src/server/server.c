28a359c764ea5bbd2a0a6fb59f1329916d047422
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-08 13:09:16 -0300

    [feat]: interfaz servidor

---
+++ Discordia/src/server/server.c
@@ -0,0 +1,6 @@
+#include "server.h"
+
+bool ds_server_init(const char* port)
+{
+    return true;
+}
---
+++ Discordia/src/server/server.h
@@ -0,0 +1,8 @@
+#ifndef DISCORDIA_SERVER_H
+#define DISCORDIA_SERVER_H
+
+#include <utils/utils.h>
+
+bool ds_server_init(const char* port);
+
+#endif
