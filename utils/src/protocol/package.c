2ea1e7c64245aaad9e141dbb909d17f999fed039
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-08 14:23:46 -0300

    [fix]: typo

--- utils/src/protocol/package.c
+++ utils/src/protocol/package.c
@@ -1,7 +1,7 @@
 #include "utils/protocol/package.h"
 #include "utils/memory/allocator.h"
 
-u_package_t* u_package_crear(u_opcode_e opcode, const u_buffer_t* buffer)
+u_package_t* u_package_create(u_opcode_e opcode, const u_buffer_t* buffer)
 {
     u_package_t* this = u_malloc(sizeof(u_package_t));
 
@@ -22,7 +22,7 @@ void u_package_delete(u_package_t* this)
     }
 }
 
-u_buffer_t* u_package_serializar(const u_package_t* paquete)
+u_buffer_t* u_package_serialize(const u_package_t* paquete)
 {
     u_buffer_t* buffer = u_buffer_create();
     uint32_t paquete_buffer_size = u_buffer_get_size(paquete->buffer);
