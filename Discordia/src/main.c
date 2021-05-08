32e78cb64ba84ded7f01bcb9964f7d9a0ffab47b
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-08 13:12:08 -0300

    [chore]: puerto de prueba en la config del Discordia

--- Discordia/src/main.c
+++ Discordia/src/main.c
@@ -1,4 +1,7 @@
 #include <utils/entry_point.h>
+#include "discordia/discordia.h"
+#include "server/server.h"
+#include "console/console.h"
 
 app_attr_t get_app_setup(void)
 {
@@ -16,6 +19,10 @@ int entry_point(int argc, char** argv)
     (void)argc; // --> para evitar el warning unused_parameter
     (void)argv; // --> para evitar el warning unused_parameter
 
-    U_LOG_INFO("Modulo Discordia");
-    return 0;
+    U_LOG_TRACE("Modulo Discordia");
+
+    if(ds_server_init("3333"))
+        ds_console_init();
+
+    return -1;
 }
