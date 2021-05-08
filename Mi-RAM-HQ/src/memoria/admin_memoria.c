c44f41d5265c39b9bececd64160338adc7d29734
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-08 18:34:40 -0300

    [feat]: interfaz administrador de memoria

---
+++ Mi-RAM-HQ/src/memoria/admin_memoria.c
@@ -0,0 +1,36 @@
+#include "admin_memoria.h"
+
+bool admin_memoria_iniciar_patota(uint32_t pid, const char* lista_tareas)
+{
+
+}
+
+bool admin_memoria_iniciar_tripulante(uint32_t pid, uint32_t tid, const u_pos_t* pos)
+{
+
+}
+
+void admin_memoria_mover_tripulante(uint32_t tid, const u_pos_t* pos)
+{
+
+}
+
+char* admin_memoria_obtener_proxima_tarea(uint32_t tid)
+{
+
+}
+
+void admin_memoria_tripulante_nuevo_estado(uint32_t tid, char estado)
+{
+
+}
+
+void admin_memoria_eliminar_tripulante(uint32_t tid)
+{
+
+}
+
+t_list* admin_memoria_obtener_tripulantes(void)
+{
+
+}
