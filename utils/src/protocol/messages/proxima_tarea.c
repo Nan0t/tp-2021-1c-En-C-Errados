238778491fc9b55aca7e5856d8be8dced5135cbc
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-16 11:15:11 -0300

    [fix]: eliminado campo pid del mensaje proxima tarea

--- utils/src/protocol/messages/proxima_tarea.c
+++ utils/src/protocol/messages/proxima_tarea.c
@@ -1,10 +1,8 @@
 #include "utils/protocol/messages/proxima_tarea.h"
 #include "utils/memory/allocator.h"
 
-u_msg_proxima_tarea_t* u_msg_proxima_tarea_crear(uint32_t _patota_id, uint32_t _tripulante_id){
+u_msg_proxima_tarea_t* u_msg_proxima_tarea_crear(uint32_t _tripulante_id){
     u_msg_proxima_tarea_t* msg = u_malloc(sizeof(u_msg_proxima_tarea_t));
-
-    msg->patota_id = _patota_id;
     msg->tripulante_id = _tripulante_id;
 
     return msg;
@@ -12,8 +10,6 @@ u_msg_proxima_tarea_t* u_msg_proxima_tarea_crear(uint32_t _patota_id, uint32_t _
 
 u_buffer_t* u_msg_proxima_tarea_serializar(const u_msg_proxima_tarea_t* _msg){
     u_buffer_t* buffer = u_buffer_create();
-
-    u_buffer_write(buffer, &_msg->patota_id, sizeof(uint32_t));
     u_buffer_write(buffer, &_msg->tripulante_id, sizeof(uint32_t));
 
     return buffer;
@@ -21,11 +17,7 @@ u_buffer_t* u_msg_proxima_tarea_serializar(const u_msg_proxima_tarea_t* _msg){
 
 u_msg_proxima_tarea_t* u_msg_proxima_tarea_deserializar(const u_buffer_t* _buffer){
     u_msg_proxima_tarea_t* proxima_tarea = u_malloc(sizeof( u_msg_proxima_tarea_t));
-    uint32_t offset = 0;
-
-    u_buffer_read(_buffer, &proxima_tarea->patota_id, sizeof(uint32_t), offset);
-    offset += sizeof(uint32_t);
-    u_buffer_read(_buffer, &proxima_tarea->tripulante_id, sizeof(uint32_t), offset);
+    u_buffer_read(_buffer, &proxima_tarea->tripulante_id, sizeof(uint32_t), 0);
 
     return proxima_tarea;
 }
@@ -37,7 +29,7 @@ void u_msg_proxima_tarea_eliminar(u_msg_proxima_tarea_t* _msg) {
 
 char* u_msg_proxima_tarea_to_string(const u_msg_proxima_tarea_t* msg) {
     return string_from_format(
-        "MSG_PROXIMA_TAREA: { PID: %d | TID: %d }",
-        msg->patota_id, msg->tripulante_id
+        "MSG_PROXIMA_TAREA: { TID: %d }",
+        msg->tripulante_id
     );
 }
