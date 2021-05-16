6fd979193fc41a51887743249ae1e0bb44793aed
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-16 11:16:47 -0300

    [fix]: correccion del tipo de dato del campo estado del mensaje tripulante nuevo estado

--- utils/src/protocol/messages/tripulante_nuevo_estado.c
+++ utils/src/protocol/messages/tripulante_nuevo_estado.c
@@ -1,7 +1,7 @@
 #include "utils/protocol/messages/tripulante_nuevo_estado.h"
 #include "utils/memory/allocator.h"
 
-u_msg_tripulante_nuevo_estado_t* u_msg_tripulante_nuevo_estado_crear(uint32_t _tripulante_id, uint32_t _nuevo_estado){
+u_msg_tripulante_nuevo_estado_t* u_msg_tripulante_nuevo_estado_crear(uint32_t _tripulante_id, char _nuevo_estado){
     u_msg_tripulante_nuevo_estado_t* msg = u_malloc(sizeof(u_msg_tripulante_nuevo_estado_t));
 
     msg->tripulante_id = _tripulante_id;
@@ -14,7 +14,7 @@ u_buffer_t* u_msg_tripulante_nuevo_estado_serializar(const u_msg_tripulante_nuev
     u_buffer_t* buffer = u_buffer_create();
 
     u_buffer_write(buffer, &_msg->tripulante_id, sizeof(uint32_t));
-    u_buffer_write(buffer, &_msg->nuevo_estado, sizeof(uint32_t));
+    u_buffer_write(buffer, &_msg->nuevo_estado, sizeof(char));
 
     return buffer;
 }
@@ -25,7 +25,7 @@ u_msg_tripulante_nuevo_estado_t* u_msg_tripulante_nuevo_estado_deserializar(cons
 
     u_buffer_read(_buffer, &tripulante_nuevo_estado->tripulante_id, sizeof(uint32_t), offset);
     offset += sizeof(uint32_t);
-    u_buffer_read(_buffer, &tripulante_nuevo_estado->nuevo_estado, sizeof(uint32_t), offset);
+    u_buffer_read(_buffer, &tripulante_nuevo_estado->nuevo_estado, sizeof(char), offset);
 
     return tripulante_nuevo_estado;
 }
@@ -37,7 +37,7 @@ void u_msg_tripulante_nuevo_estado_eliminar(u_msg_tripulante_nuevo_estado_t* _ms
 
 char* u_msg_tripulante_nuevo_estado_to_string(const u_msg_tripulante_nuevo_estado_t* msg) {
     return string_from_format(
-        "MSG_TRIPULANTE_NUEVO_ESTADO: { TID: %d | Estado: %d }",
+        "MSG_TRIPULANTE_NUEVO_ESTADO: { TID: %d | Estado: %c }",
         msg->tripulante_id, msg->nuevo_estado
     );
 }
