71ab9c7b812936d096715b6adc0aa26baa4d613b
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-19 13:31:57 +0000

    [feat]: ahora las funciones de lectura y escritura de los bloques retornan la cantidad de bytes escritos/leidos.

--- I-Mongo-Store/src/fs/block.c
+++ I-Mongo-Store/src/fs/block.c
@@ -20,14 +20,26 @@ void fs_block_delete(fs_block_t* this)
     u_free(this);
 }
 
-void fs_block_write(fs_block_t* this, const void* data, uint64_t data_size, uint64_t offset)
+uint32_t fs_block_write(fs_block_t* this, const void* data, uint64_t data_size, uint64_t offset)
 {
-    U_ASSERT(data_size + offset < this->block_size, "Trying to write OutOfBound block location");
-    fs_physical_disk_write(data, data_size, this->disk_offset + offset);
+    uint32_t bytes_to_write =
+        (data_size + offset < this->block_size) ?
+            data_size :
+            data_size - (data_size + offset - this->block_size);
+
+    fs_physical_disk_write(data, bytes_to_write, this->disk_offset + offset);
+
+    return bytes_to_write;
 }
 
-void fs_block_read(fs_block_t* this, void* data, uint64_t data_size, uint64_t offset)
+uint32_t fs_block_read(fs_block_t* this, void* data, uint64_t data_size, uint64_t offset)
 {
-    U_ASSERT(data_size + offset < this->block_size, "Trying to read OutOfBound block location");
-    fs_physical_disk_read(data, data_size, this->disk_offset + offset);
+    uint32_t bytes_to_read =
+        (data_size + offset < this->block_size) ?
+            data_size :
+            data_size - (data_size + offset - this->block_size);
+
+    fs_physical_disk_read(data, bytes_to_read, this->disk_offset + offset);
+
+    return bytes_to_read;
 }
--- I-Mongo-Store/src/fs/block.h
+++ I-Mongo-Store/src/fs/block.h
@@ -5,7 +5,7 @@
 
 typedef struct fs_block_t fs_block_t;
 
-void fs_block_write(fs_block_t* block, const void* data, uint64_t data_size, uint64_t offset);
-void fs_block_read(fs_block_t* block, void* data, uint64_t data_size, uint64_t offset);
+uint32_t fs_block_write(fs_block_t* block, const void* data, uint64_t data_size, uint64_t offset);
+uint32_t fs_block_read(fs_block_t* block, void* data, uint64_t data_size, uint64_t offset);
 
 #endif
