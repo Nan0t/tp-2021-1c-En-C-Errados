8ac65f812ae333d50dba732b606ba4e8bd9c5d1e
Hernan Solis <hsolis@frba.utn.edu.ar>
2021-05-20 13:02:41 -0300

    refactor: tareas separadas de tripulante

--- Discordia/src/planificador/tripulante.h
+++ Discordia/src/planificador/tripulante.h
@@ -3,15 +3,8 @@
 
 #include <utils/utils.h>
 #include <semaphore.h>
+#include "tareas.h"
 
-typedef struct
-{
-    char*    tarea;
-    bool     is_blocking; //avisa si debe ejecutar en CPU o pasar a lista bloqueados
-    bool     is_finished;
-    uint32_t tiempo_bloqueado;
-    u_pos_t  pos;
-} tarea_t;
 
 typedef struct
 {
