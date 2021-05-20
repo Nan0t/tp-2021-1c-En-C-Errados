8e8eb6c33a0463e627f486422324d158292d3787
Hernan Solis <hsolis@frba.utn.edu.ar>
2021-05-20 12:59:38 -0300

    feat: manejo lista de tripulantes

---
+++ Discordia/src/planificador/queues/block_tripulantes_list.c
@@ -0,0 +1,52 @@
+#include "block_tripulantes_list.h"
+#include <semaphore.h>
+#include <pthread.h>
+
+private pthread_mutex_t p_block_tripulantes_list_mx = PTHREAD_MUTEX_INITIALIZER;
+private sem_t           p_block_tripulantes_list_sem;
+private t_list*         p_block_tripulantes_list    = NULL;
+
+void ds_block_tripulantes_init(void)
+{
+    if(p_block_tripulantes_list)
+        return;
+
+    p_block_tripulantes_list = list_create();
+    sem_init(&p_block_tripulantes_list_sem, 0, 0);
+}
+
+void ds_block_tripulantes_add(tripulante_t* trip)
+{
+    pthread_mutex_lock(&p_block_tripulantes_list_mx);
+    list_add(p_block_tripulantes_list, trip);
+    pthread_mutex_unlock(&p_block_tripulantes_list_mx);
+
+    sem_post(&p_block_tripulantes_list_sem);
+}
+
+tripulante_t* ds_block_tripulantes_get_and_remove(void)
+{
+    if(sem_trywait(&p_block_tripulantes_list_sem) == -1)
+        return NULL;
+        
+    pthread_mutex_lock(&p_block_tripulantes_list_mx);
+    tripulante_t* trip = list_remove(p_block_tripulantes_list,0);
+    pthread_mutex_unlock(&p_block_tripulantes_list_mx);
+
+    return trip;
+}
+
+void ds_block_tripulantes_iterate(void(*closure)(tripulante_t*))
+{
+  list_iterate(p_block_tripulantes_list, (void*)closure);
+}
+
+bool ds_block_tareas_any_satisfy(bool(*closure)(tripulante_t*))
+{
+   return list_any_satisfy(p_block_tripulantes_list, (void*)closure);
+}
+
+tripulante_t* ds_block_tripulantes_remove_by_condition(bool(*closure)(tripulante_t*))
+{
+    return list_remove_by_condition(p_block_tripulantes_list, (void*)closure);
+}
---
+++ Discordia/src/planificador/queues/block_tripulantes_list.h
@@ -0,0 +1,14 @@
+#ifndef PLANIFICADOR_BLOCK_TRIPULANTES_H
+#define PLANIFICADOR_BLOCK_TRIPULANTES_H
+
+#include "../tripulante.h"
+
+void ds_block_tripulantes_init(void);
+
+void          ds_block_tripulantes_add(tripulante_t* trip);
+tripulante_t* ds_block_tripulantes_get_and_remove(void);
+void ds_block_tripulantes_iterate(void(*closure)(tripulante_t*));
+bool ds_block_tareas_any_satisfy(bool(*closure)(tripulante_t*));
+tripulante_t* ds_block_tripulantes_remove_by_condition(bool(*closure)(tripulante_t*));
+
+#endif
