a037f2097fbba24141abd2ec3e6e99dd0e586ee3
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-21 00:56:34 +0000

    [feat]: implementación loop planificador

--- Discordia/src/planificador/planificador.c
+++ Discordia/src/planificador/planificador.c
@@ -1,21 +1,46 @@
 #include "discordia/discordia.h"
+
 #include "planificador.h"
+#include "synchronizer.h"
+#include "io.h"
+
+#include "queues/block_sabotaje_queue.h"
+#include "queues/block_tripulantes_list.h"
+#include "queues/exec_queue.h"
 #include "queues/new_queue.h"
+#include "queues/ready_queue.h"
+
 #include "cpu.h"
+
+#include <pthread.h>
+
+#include <errno.h>
+#include <string.h>
+
+typedef void(*ds_algo_sched_func)(tripulante_t*);
 
 typedef struct
 {
     uint32_t cant_cpu;
     uint32_t quantum;
     uint32_t duracion_sabotaje;
-    uint32_t retardo_ciclo_cpu;
-    char*    algoritmo;
-
-    pthread_mutex_t esta_pausado_mx;
-    bool            esta_pausado;
+    ds_algo_sched_func algorithm;
 } planificador_t;
 
 private planificador_t* p_planificador = NULL;
+
+private void ds_algorithm_fifo(tripulante_t* trip);
+private void ds_algorithm_rr(tripulante_t* trip);
+
+private ds_algo_sched_func ds_planificador_select_algorithm(void);
+
+private void ds_planificador_init_queues();
+private void ds_planificador_init_devices();
+
+private void ds_planificador_loop(void);
+
+private void ds_planificador_admit_from_new_to_ready(void);
+private void ds_planificador_check_exec_queue(void);
 
 void planificador_init(void)
 {
@@ -24,21 +49,19 @@ void planificador_init(void)
 
     p_planificador = u_malloc(sizeof(planificador_t));
 
-    p_planificador->cant_cpu = u_config_get_int_value("GRADO_MULTITAREA");
-    p_planificador->quantum  = u_config_get_int_value("QUANTUM");
+    p_planificador->cant_cpu          = u_config_get_int_value("GRADO_MULTITAREA");
+    p_planificador->quantum           = u_config_get_int_value("QUANTUM");
     p_planificador->duracion_sabotaje = u_config_get_int_value("DURACION_SABOTAJE");
-    p_planificador->retardo_ciclo_cpu = u_config_get_int_value("RETARDO_CICLO_CPU");
-    p_planificador->algoritmo = u_config_get_string_value("ALGORITMO");
+    p_planificador->algorithm         = ds_planificador_select_algorithm();
 
-    pthread_t thread_cpu;
-    
-    for(uint32_t i = 0; i < p_planificador->cant_cpu; i ++)
-    {
-        U_ASSERT(pthread_create(&thread_cpu, NULL, (void*)cpu_init, NULL) != -1,
-            "No se pudo crear el hilo de la CPU");
+    ds_planificador_init_queues();
+    ds_planificador_init_devices();
 
-        pthread_detach(thread_cpu);
-    }
+    pthread_t thread_planificador;
+    U_ASSERT(pthread_create(&thread_planificador, NULL, (void*)ds_planificador_loop, NULL) != -1,
+        "No se pudo crear el hilo del Planificador: %s", strerror(errno));
+
+    pthread_detach(thread_planificador);
 }
 
 void planificador_iniciar_tripulante(uint32_t tid, const u_pos_t* pos)
@@ -59,9 +82,115 @@ void planificador_iniciar_tripulante(uint32_t tid, const u_pos_t* pos)
 
 bool planificador_esta_pausado(void)
 {
-    pthread_mutex_lock(&p_planificador->esta_pausado_mx);
-    bool pausar = p_planificador->esta_pausado;
-    pthread_mutex_unlock(&p_planificador->esta_pausado_mx);
+    return true;
+}
 
-    return pausar;
+private void ds_algorithm_fifo(tripulante_t* trip)
+{
+    if(trip->bloquear)
+    {
+        trip->bloquear = false;
+        ds_block_tripulantes_add(trip);
+    }
+    else
+        ds_exec_queue_push(trip);
+}
+
+private void ds_algorithm_rr(tripulante_t* trip)
+{
+    trip->quatum --;
+
+    if(trip->bloquear)
+    {
+        trip->bloquear = false;
+        trip->quatum = p_planificador->quantum;
+        ds_block_tripulantes_add(trip);
+    }
+    else if(trip->quatum == 0)
+    {
+        trip->quatum = p_planificador->quantum;
+        ds_block_tripulantes_add(trip);
+    }
+    else
+        ds_exec_queue_push(trip);
+}
+
+private ds_algo_sched_func ds_planificador_select_algorithm(void)
+{
+    const char* algorithm_str = u_config_get_string_value("ALGORITMO");
+
+    if(!strcmp(algorithm_str, "RR"))
+        return ds_algorithm_rr;
+
+    return ds_algorithm_fifo;
+}
+
+private void ds_planificador_init_queues()
+{
+    ds_new_queue_init();
+    ds_ready_queue_init();
+    ds_block_sabotaje_init();
+    ds_block_tripulantes_init();
+    ds_exec_queue_init(p_planificador->cant_cpu);
+}
+
+private void ds_planificador_init_devices()
+{
+    ds_synchronizer_init(p_planificador->cant_cpu + 1);
+
+    pthread_t thread_id;
+    pthread_attr_t thread_attr;
+
+    pthread_attr_init(&thread_attr);
+    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
+
+    for(uint32_t i = 0; i < p_planificador->cant_cpu; i ++)
+        U_ASSERT(pthread_create(&thread_id, &thread_attr, (void*)cpu_init, NULL) != -1,
+            "No se pudo crear el hilo del CPU %d: %s", i, strerror(errno));
+
+    U_ASSERT(pthread_create(&thread_id, &thread_attr, (void*)io_init, NULL) != -1,
+        "No se pudo crear el hilo de I/O: %s", strerror(errno));
+}
+
+extern void ds_synchronizer_sched_notify_new_cycle(void);
+extern void ds_synchronizer_sched_wait_end_of_cicle(void);
+
+private void ds_planificador_loop(void)
+{
+    while(1)
+    {
+        ds_planificador_admit_from_new_to_ready();
+        ds_planificador_check_exec_queue();
+
+        ds_synchronizer_sched_notify_new_cycle();
+        ds_synchronizer_sched_wait_end_of_cicle();
+    }
+}
+
+private void ds_planificador_admit_from_new_to_ready(void)
+{
+    tripulante_t* trip = ds_new_queue_pop();
+
+    while(trip != NULL)
+    {
+        ds_ready_queue_push(trip);
+        trip = ds_new_queue_pop();
+    }
+}
+
+private void ds_planificador_check_exec_queue(void)
+{
+    uint64_t exec_queue_size = ds_exec_queue_size();
+
+    for(uint64_t i = 0; i < exec_queue_size; i ++)
+    {
+        tripulante_t* trip = ds_exec_queue_pop();
+        p_planificador->algorithm(trip);
+    }
+
+    while(!ds_exec_is_full())
+    {
+        tripulante_t* trip = ds_ready_queue_pop();
+        ds_exec_queue_push(trip);
+    }
 }
--- Discordia/src/planificador/planificador.h
+++ Discordia/src/planificador/planificador.h
@@ -4,12 +4,10 @@
 #include "tripulante/tripulante.h"
 #include <utils/utils.h>
 
-void planificador_init(void);
-void planificador_iniciar_tripulante(uint32_t tid, const u_pos_t* pos);
+void ds_planificador_init(void);
+void ds_planificador_iniciar_tripulante(uint32_t tid, const u_pos_t* pos);
 
-void planificador_iniciar(void);
-void planificador_pausar(void);
-
-bool planificador_esta_pausado(void);
+void ds_planificador_iniciar(void);
+void ds_planificador_pausar(void);
 
 #endif
