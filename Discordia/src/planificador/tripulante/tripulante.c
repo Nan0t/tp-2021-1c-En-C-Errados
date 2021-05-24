5833875bc81146a0bbf3e94220039bdd79216f32
Hernan Solis <hsolis@frba.utn.edu.ar>
2021-05-24 17:32:30 -0300

    [feat|refactor] delegación y movimiento tripulante

--- Discordia/src/planificador/tripulante/tripulante.c
+++ Discordia/src/planificador/tripulante/tripulante.c
@@ -5,6 +5,8 @@
 #define CMP_POS(pos1, pos2) (pos1.x == pos2.x && pos1.y == pos2.y)
 
 private void tripulante_loop(tripulante_t* trip_info);
+private void tripulante_mover(tripulante_t* tripulante);
+private void tripulante_obtener_proxima_tarea(tripulante_t* tripulante);
 
 
 
@@ -18,9 +20,7 @@ void tripulante_init(tripulante_t* trip_info)
 private void tripulante_loop(tripulante_t* trip_info)
 {
     bool termino_hilo = false;
-    char* proxima_tarea = discordia_obtener_proxima_tarea(trip_info->tid);
-
-    trip_info->tarea_actual = parsear_tarea(proxima_tarea);
+    tripulante_obtener_proxima_tarea(trip_info);
 
     while(!termino_hilo)
     {
@@ -28,32 +28,69 @@ private void tripulante_loop(tripulante_t* trip_info)
 
         if(trip_info->tarea_actual->is_finished)
         {
-            char* proxima_tarea = discordia_obtener_proxima_tarea(trip_info->tid);
+            discordia_finalizar_tarea(trip_info->tid, trip_info->tarea_actual);
+            tripulante_obtener_proxima_tarea(trip_info);
 
-            if(proxima_tarea == NULL)
+            if(trip_info->tarea_actual == NULL)
             {
                 termino_hilo = true;
                 discordia_expulsar_tripulante(trip_info->tid);
             }
             else
-            {
-                trip_info->tarea_actual = parsear_tarea(proxima_tarea);
-            }
+                discordia_desplazamiento_tripulante(trip_info->tid, &trip_info->pos, &trip_info->tarea_actual->pos);
         }
         
-        if(!termino_hilo && CMP_POS(trip_info->pos, trip_info->tarea_actual->pos))
+        if(!termino_hilo)
         {
-            if(trip_info->tarea_actual->is_blocking)
-                trip_info->bloquear = true;
+            if(CMP_POS(trip_info->pos, trip_info->tarea_actual->pos))
+            {
+                if(trip_info->tarea_actual->is_blocking)
+                    trip_info->bloquear = true;
+                else
+                {
+                    trip_info->tarea_actual->tiempo_bloqueado --;
+                    trip_info->tarea_actual->is_finished = trip_info->tarea_actual->tiempo_bloqueado == 0;
+                }
+            }
             else
             {
-                trip_info->tarea_actual->tiempo_bloqueado --;
-                trip_info->tarea_actual->is_finished = trip_info->tarea_actual->tiempo_bloqueado == 0;
+                tripulante_mover(trip_info);
             }
+        }  
 
-            discordia_iniciar_tarea(trip_info->tid, trip_info->tarea_actual->tarea);
-        }
-        
         sem_post(&trip_info->sem_end_exec);
     }
+}
+
+private void tripulante_mover(tripulante_t* tripulante)
+{
+
+	if (tripulante->pos.x != tripulante->tarea_actual->pos.x) 
+    {
+		if(tripulante->pos.x < tripulante->tarea_actual->pos.x) 
+        {
+			tripulante->pos.x ++;
+		} 
+        else 
+        {
+			tripulante->pos.x --;
+		}
+	}
+    else if (tripulante->pos.y != tripulante->tarea_actual->pos.y)
+    {
+		if(tripulante->pos.y < tripulante->tarea_actual->pos.y) 
+        {
+			tripulante->pos.y ++;
+		} 
+        else 
+        {
+			tripulante->pos.y --;
+		}
+	}
+    discordia_mover_tripulante(tripulante->tid, &tripulante->pos);
+}
+
+private void tripulante_obtener_proxima_tarea(tripulante_t* tripulante){
+    char* proxima_tarea = discordia_obtener_proxima_tarea(tripulante->tid);
+    tripulante->tarea_actual = parsear_tarea(proxima_tarea);
 }
