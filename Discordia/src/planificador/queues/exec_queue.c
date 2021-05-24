85fd385a1c651e909d6e9c2f965db2c7693f9881
int0x80-sys <masosa@frba.utn.edu.ar>
2021-05-24 22:38:24 +0000

    [feat|fix]: reorganización de la estructura interna de la cola exec. Corrección función is_full

--- Discordia/src/planificador/queues/exec_queue.c
+++ Discordia/src/planificador/queues/exec_queue.c
@@ -2,52 +2,64 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-private pthread_mutex_t p_exec_queue_mx = PTHREAD_MUTEX_INITIALIZER;
-private t_queue*        p_exec_queue    = NULL;
+typedef struct
+{
+    uint32_t curr_elements;
+    uint32_t max_elements;
 
-private sem_t           p_exec_queue_sem;
-private sem_t           p_exec_queue_full_sem;
+    pthread_mutex_t mx;
+
+    t_queue*  queue;
+} ds_exec_queue_t;
+
+private ds_exec_queue_t* p_exec_queue = NULL;
 
 void ds_exec_queue_init(uint32_t max_size)
 {
     if(p_exec_queue)
         return;
 
-    p_exec_queue = queue_create();
-    sem_init(&p_exec_queue_sem, 0, 0);
-    sem_init(&p_exec_queue_full_sem, 0, max_size);
+    p_exec_queue = u_malloc(sizeof(ds_exec_queue_t));
+
+    p_exec_queue->curr_elements = 0;
+    p_exec_queue->max_elements  = max_size;
+    p_exec_queue->queue         = queue_create();
+
+    pthread_mutex_init(&p_exec_queue->mx, NULL);
 }
 
 bool ds_exec_is_full(void)
 {
-    return sem_trywait(&p_exec_queue_full_sem) != -1;
+    pthread_mutex_lock(&p_exec_queue->mx);
+    bool is_full = p_exec_queue->curr_elements == p_exec_queue->max_elements;
+    pthread_mutex_unlock(&p_exec_queue->mx);
+
+    return is_full;
 }
 
 void ds_exec_queue_push(tripulante_t* trip)
 {
-    sem_wait(&p_exec_queue_full_sem);
-    pthread_mutex_lock(&p_exec_queue_mx);
-    queue_push(p_exec_queue, trip);
-    pthread_mutex_unlock(&p_exec_queue_mx);
-
-    sem_post(&p_exec_queue_sem);
+    pthread_mutex_lock(&p_exec_queue->mx);
+    queue_push(p_exec_queue->queue, trip);
+    pthread_mutex_unlock(&p_exec_queue->mx);
 }
 
 tripulante_t* ds_exec_queue_pop(void)
 {
-    if(sem_trywait(&p_exec_queue_sem) == -1)
-        return NULL;
-        
-    pthread_mutex_lock(&p_exec_queue_mx);
-    tripulante_t* trip = queue_pop(p_exec_queue);
-    pthread_mutex_unlock(&p_exec_queue_mx);
-
-    sem_post(&p_exec_queue_full_sem);
+    pthread_mutex_lock(&p_exec_queue->mx);
+    if(p_exec_queue->curr_elements == 0)
+    {
+        pthread_mutex_unlock(&p_exec_queue->mx);
+        return false;
+    }
+    
+    tripulante_t* trip = queue_pop(p_exec_queue->queue);
+    pthread_mutex_unlock(&p_exec_queue->mx);
 
     return trip;
 }
 
 uint64_t ds_exec_queue_size(void)
 {
-    return queue_size(p_exec_queue);
+    return queue_size(p_exec_queue->queue);
 }
