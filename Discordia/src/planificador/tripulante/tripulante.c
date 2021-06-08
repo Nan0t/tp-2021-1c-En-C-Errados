444a336d61e6c5de45f45f074696eb0c6b587f71
int0x80-sys <masosa@frba.utn.edu.ar>
2021-06-08 22:10:00 +0000

    [refactory]: se le agrega el estado actual a la estructura tripulante. Nueva función para cambiar el estado de un tripulante. Se mueven los logs relacionados con el tripulante dentro de tripulante.c. Reorganización del loop principal.

--- Discordia/src/planificador/tripulante/tripulante.c
+++ Discordia/src/planificador/tripulante/tripulante.c
@@ -1,11 +1,33 @@
 #include "discordia/discordia.h"
 #include "tripulante.h"
 #include <pthread.h>
+#include <errno.h>
 
 #define CMP_POS(pos1, pos2) (pos1.x == pos2.x && pos1.y == pos2.y)
 
+private const char STATES_CHAR[] =
+{
+    'N',
+    'R',
+    'E',
+    'B',
+    'B'
+};
+
+private const char* STATES_STR[] =
+{
+    "NEW",
+    "READY",
+    "EXEC",
+    "BLOCK I/O",
+    "BLOCK SABOTAGE",
+    "EXIT"
+};
+
 private void tripulante_loop(tripulante_t* this);
-private void tripulante_mover(tripulante_t* tripulante);
+private void tripulante_mover(tripulante_t* this);
+private bool tripulante_check_tarea_actual(tripulante_t* this);
+private void tripulante_free(tripulante_t* this);
 
 tripulante_t* tripulante_create(uint32_t pid, uint32_t tid, const u_pos_t* pos, uint32_t quantum)
 {
@@ -16,30 +38,50 @@ tripulante_t* tripulante_create(uint32_t pid, uint32_t tid, const u_pos_t* pos, 
     this->pos.x        = pos->x;
     this->pos.y        = pos->y;
     this->quatum       = quantum;
+    this->curr_state   = TRIP_STATE_NEW;
+    this->terminate    = false;
     this->bloquear     = false;
     this->tarea_actual = NULL;
     
     sem_init(&this->sem_sync, 0, 0);
     sem_init(&this->sem_end_exec, 0, 0);
 
+    tripulante_obtener_proxima_tarea(this);
+
     return this;
 }
 
 void tripulante_delete(tripulante_t* this)
 {
+    discordia_eliminar_tripulante(this->pid, this->tid);
+
     sem_destroy(&this->sem_sync);
     sem_destroy(&this->sem_end_exec);
+
     u_free(this);
 }
 
-bool tripulante_obtener_proxima_tarea(tripulante_t* tripulante){
-    char* proxima_tarea = discordia_obtener_proxima_tarea(tripulante->pid, tripulante->tid);
+void tripulante_change_state(tripulante_t* this, trip_state_e state)
+{
+    if(state >= TRIP_STATE_NEW && state <= TRIP_STATE_BLOCK_SABOTAGE)
+        discordia_tripulante_nuevo_estado(this->pid, this->tid, STATES_CHAR[state]);
 
-    if(proxima_tarea == NULL)
+    U_LOG_INFO("Tripulante %d pasa de estado %s a %s",
+        this->tid, STATES_STR[this->curr_state], STATES_STR[state]);
+
+    this->curr_state = state;
+}
+
+bool tripulante_obtener_proxima_tarea(tripulante_t* this){
+    char* proxima_tarea = discordia_obtener_proxima_tarea(this->pid, this->tid);
+    
+    eliminar_tarea(this->tarea_actual);
+    this->tarea_actual = parsear_tarea(proxima_tarea);
+
+    if(this->tarea_actual == NULL)
         return false;
 
-    eliminar_tarea(tripulante->tarea_actual);
-    tripulante->tarea_actual = parsear_tarea(proxima_tarea);
+    u_free(proxima_tarea);
 
     return true;
 }
@@ -47,86 +89,99 @@ bool tripulante_obtener_proxima_tarea(tripulante_t* tripulante){
 void tripulante_init(tripulante_t* this)
 {
     pthread_t tripulante_thread;
-    pthread_create(&tripulante_thread, NULL, (void*)tripulante_loop, this);
+    U_ASSERT(pthread_create(&tripulante_thread, NULL, (void*)tripulante_loop, this) != -1,
+        "No se pudo crear un hilo nuevo para el tripulante %d: %s", this->tid, strerror(errno));
     pthread_detach(tripulante_thread);
+
+    U_LOG_DEBUG("Hilo del tripulante %d inicializado", this->tid);
+}
+
+void tripulante_terminate(tripulante_t* this)
+{
+    U_LOG_INFO("Tripulante %d pasa de %s a EXIT", this->tid, STATES_STR[this->curr_state]);
+    discordia_eliminar_tripulante(this->pid, this->tid);
+    this->terminate = true;
+    sem_post(&this->sem_sync);
 }
 
 private void tripulante_loop(tripulante_t* this)
 {
-    bool termino_hilo = false;
-    tripulante_obtener_proxima_tarea(this);
-
-    while(!termino_hilo)
+    while(!this->terminate)
     {
         sem_wait(&this->sem_sync);
 
-        if(this->tarea_actual->is_finished)
-        {
-            discordia_finalizar_tarea(this->tid, this->tarea_actual->tarea);
+        if(this->terminate)
+            continue;
 
-            if(!tripulante_obtener_proxima_tarea(this))
-            {
-                termino_hilo = true;
-                discordia_expulsar_tripulante(this->tid);
-            }
-            else
-                discordia_desplazamiento_tripulante(this->tid, &this->pos, &this->tarea_actual->pos);
-        }
-        
-        if(!termino_hilo)
-        {
-            if(CMP_POS(this->pos, this->tarea_actual->pos))
-            {
-                if(this->tarea_actual->is_blocking)
-                    this->bloquear = true;
-                else
-                {
-                    this->tarea_actual->tiempo_bloqueado --;
-                    U_LOG_INFO("Tripulante %d ejecuta tarea %s. Tiempo restante: %d",
-                        this->tid, this->tarea_actual->tarea, this->tarea_actual->tiempo_bloqueado);
-
-                    this->tarea_actual->is_finished = this->tarea_actual->tiempo_bloqueado == 0; //TODO: PEDIR NUEVA TAREA Y MANEJAR SI DEVUELVE NULL
-                }
-            }
-            else
-            {
-                tripulante_mover(this);
-            }
-        }  
+        if(tripulante_check_tarea_actual(this))
+            tripulante_mover(this);
 
         sem_post(&this->sem_end_exec);
     }
 
-    tripulante_delete(this);
+    tripulante_free(this);
 }
 
-private void tripulante_mover(tripulante_t* tripulante)
+private void tripulante_mover(tripulante_t* this)
 {
-    u_pos_t prev_pos = { .x = tripulante->pos.x, .y = tripulante->pos.y };
+    u_pos_t prev_pos = { .x = this->pos.x, .y = this->pos.y };
 
-	if (tripulante->pos.x != tripulante->tarea_actual->pos.x) 
+	if (this->pos.x != this->tarea_actual->pos.x) 
     {
-		if(tripulante->pos.x < tripulante->tarea_actual->pos.x) 
-        {
-			tripulante->pos.x ++;
-		} 
-        else 
-        {
-			tripulante->pos.x --;
-		}
+		if(this->pos.x < this->tarea_actual->pos.x)
+			this->pos.x ++;
+        else
+			this->pos.x --;
 	}
-    else if (tripulante->pos.y != tripulante->tarea_actual->pos.y)
+    else if (this->pos.y != this->tarea_actual->pos.y)
     {
-		if(tripulante->pos.y < tripulante->tarea_actual->pos.y) 
-        {
-			tripulante->pos.y ++;
-		} 
-        else 
-        {
-			tripulante->pos.y --;
-		}
+		if(this->pos.y < this->tarea_actual->pos.y)
+			this->pos.y ++;
+        else
+			this->pos.y --;
 	}
-    discordia_mover_tripulante(tripulante->pid, tripulante->tid, &tripulante->pos);
+
+    discordia_mover_tripulante(this->pid, this->tid, &this->pos);
     U_LOG_INFO("Tripulante %d se mueve de { %d, %d } a { %d, %d }",
-        tripulante->tid, prev_pos.x, prev_pos.y, tripulante->pos.x, tripulante->pos.y);
+        this->tid, prev_pos.x, prev_pos.y, this->pos.x, this->pos.y);
+}
+
+private bool tripulante_check_tarea_actual(tripulante_t* this)
+{
+    if(this->tarea_actual->is_finished)
+    {
+        if(!tripulante_obtener_proxima_tarea(this))
+            discordia_expulsar_tripulante(this->tid);
+            
+        return false;
+    }
+
+    if(CMP_POS(this->pos, this->tarea_actual->pos))
+    {
+        if(this->tarea_actual->is_blocking)
+        {
+            discordia_iniciar_tarea(this->tid, this->tarea_actual->tarea);
+            this->bloquear = true;
+        }
+        else
+        {
+            this->tarea_actual->tiempo_bloqueado --;
+            U_LOG_INFO("Tripulante %d ejecuta tarea %s. Tiempo restante: %d",
+                this->tid, this->tarea_actual->tarea, this->tarea_actual->tiempo_bloqueado);
+
+            this->tarea_actual->is_finished = this->tarea_actual->tiempo_bloqueado == 0;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+private void tripulante_free(tripulante_t* this)
+{
+    eliminar_tarea(this->tarea_actual);
+    sem_destroy(&this->sem_sync);
+    sem_destroy(&this->sem_end_exec);
+    u_free(this);
 }
