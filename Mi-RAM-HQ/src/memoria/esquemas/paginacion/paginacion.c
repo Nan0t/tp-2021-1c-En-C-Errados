c78ef02ac89b111652b992b7424db24e3f1e1e79
MartinGongora <martin_gongora@hotmail.com>
2021-06-09 17:03:23 -0300

    [fix]: arreglado inicializar_patota

--- Mi-RAM-HQ/src/memoria/esquemas/paginacion/paginacion.c
+++ Mi-RAM-HQ/src/memoria/esquemas/paginacion/paginacion.c
@@ -120,55 +120,62 @@ private void paginacion_agregar_patota_a_listado(uint32_t pid, uint32_t cant_tri
 }
 
 private bool paginacion_agregar_patota_en_memoria(uint32_t pid, uint32_t cant_tripulantes, const char* tareas, int tamanio_data){
-    uint32_t pid_y_direccion_tareas[2] = {pid, 21 * cant_tripulantes};
+    uint32_t pid_y_direccion_tareas[2] = {pid, 8 + 21 * cant_tripulantes};
     int offset_general = 0;
     int i;
     int j;
     p_frame_t* frame_a_escribir = list_find(lista_frames_memoria, paginacion_frame_esta_libre);
-    
+    int numero_de_frame = frame_a_escribir->num_frame;
     //escribo el pid y direccion logica inicio de tareas
     for(i = 0; i<2; i++){
-        memcpy(esquema_memoria_mfisica + offset_general, &pid_y_direccion_tareas[i], sizeof(uint32_t));//ver si estoy copiando bien
+        memcpy(esquema_memoria_mfisica + numero_de_frame*tamanio_pagina + offset_general, &pid_y_direccion_tareas[i], sizeof(uint32_t));//ver si estoy copiando bien
         offset_general = offset_general + sizeof(uint32_t);
-        if(offset_general = tamanio_pagina){ 
+        if(offset_general == tamanio_pagina){ 
             frame_a_escribir = paginacion_cambiar_frame(frame_a_escribir); // esto anda bien?  OJO; 
+            numero_de_frame = frame_a_escribir->num_frame;
             offset_general = 0;
         }
     }
-    U_LOG_TRACE("Pid: %d, y direccion de tareas:%d copiado con exito", pid_y_direccion_tareas[0], pid_y_direccion_tareas[1]);
+    U_LOG_TRACE("Pid: %d, y direccion de tareas: %d copiado con exito", pid_y_direccion_tareas[0], pid_y_direccion_tareas[1]);
     //hago espacio para tripulantes y escribo proxima tarea y direccion logica del pcb
     uint32_t prox_tarea_y_direccion_pcb[2] = {0, 0}; //direccion logica del pcb es 0?
     for(i=0; i<cant_tripulantes; i++){
         for(j=0; j<13; j++){
             offset_general++;
-            if(offset_general = tamanio_pagina){
+            if(offset_general == tamanio_pagina){
                 frame_a_escribir = paginacion_cambiar_frame(frame_a_escribir); 
                 offset_general = 0;
+                numero_de_frame = frame_a_escribir->num_frame;
             }        
         }
         for(j = 0; j < 2; j++){
             if((offset_general + sizeof(uint32_t)) > tamanio_pagina){
                 frame_a_escribir = paginacion_cambiar_frame(frame_a_escribir);
                 offset_general = 0;
+                numero_de_frame = frame_a_escribir->num_frame;
             }    
-            memcpy(esquema_memoria_mfisica + offset_general, &prox_tarea_y_direccion_pcb[j], sizeof(uint32_t));
+            memcpy(esquema_memoria_mfisica + numero_de_frame*tamanio_pagina + offset_general, &prox_tarea_y_direccion_pcb[j], sizeof(uint32_t));
+            offset_general = offset_general + sizeof(uint32_t);
         }
-        if(offset_general = tamanio_pagina){
+        if(offset_general == tamanio_pagina){
             frame_a_escribir = paginacion_cambiar_frame(frame_a_escribir); 
             offset_general = 0;
+            numero_de_frame = frame_a_escribir->num_frame;
         } 
     }
     U_LOG_TRACE("Proxima tarea y direccion pcb escrito con exito");
     //escribo las tareas
     int offset_tareas = 0;
     while(offset_tareas < strlen(tareas)+1){
-        int start = tamanio_pagina * frame_a_escribir->num_frame + offset_general; 
-        int end = start + minimo(tamanio_pagina - offset_general, offset_tareas);
+        int start = tamanio_pagina * numero_de_frame + offset_general; 
+        int end = start + minimo(tamanio_pagina - offset_general, strlen(tareas)+1);
         for(i=start; i<end; i++){
             esquema_memoria_mfisica[i] = tareas[offset_tareas];
             offset_tareas++;
+            offset_general++;
+            U_LOG_TRACE("Escrito caracter: %d", i);
         }
-        if(offset_general = tamanio_pagina){
+        if(offset_general == tamanio_pagina){
             frame_a_escribir = paginacion_cambiar_frame(frame_a_escribir); 
             offset_general = 0;
         } 
