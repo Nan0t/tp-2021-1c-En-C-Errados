22dc2cf7b1b15ada9836903277f5df7e316574e5
int0x80-sys <masosa@frba.utn.edu.ar>
2021-06-10 14:32:06 +0000

    [feat]: integración del file system con los administradores de bloques y files

--- I-Mongo-Store/src/fs/file_system.c
+++ I-Mongo-Store/src/fs/file_system.c
@@ -1,11 +1,32 @@
 #include "file_system.h"
+#include "blocks/blocks_manager.h"
+#include "bitacora/bitacoras_manager.h"
+#include "file/files_manager.h"
 #include "sabotage/sabotage_notifier.h"
+#include "task_parser/task_parser.h"
+
 #include <pthread.h>
 
-private void  file_system_create_bitacora(uint32_t tid);
-private void  file_system_remove_bitacora(uint32_t tid);
+typedef void(*fs_task_exec_func)(const fs_task_parser_result_t*);
+
 private void  file_system_update_bitacora(uint32_t tid, const char* content);
 private char* file_system_get_bitacora(uint32_t tid);
+
+private void file_system_execute_task(const char* task);
+
+private void file_system_add_char_fill(const fs_task_parser_result_t* task);
+private void file_system_rm_char_fill(const fs_task_parser_result_t* task);
+private void file_system_rm_file(const fs_task_parser_result_t* task);
+
+private const fs_task_exec_func TASK_EXEC_FUNCS[] =
+{
+    file_system_add_char_fill,
+    file_system_rm_char_fill,
+    file_system_add_char_fill,
+    file_system_rm_char_fill,
+    file_system_add_char_fill,
+    file_system_rm_file,
+};
 
 void file_system_init(const file_system_attr_t* attr)
 {
@@ -13,6 +34,9 @@ void file_system_init(const file_system_attr_t* attr)
     U_LOG_INFO("Punto de Montaje: %s", attr->mount_point);
     U_LOG_INFO("Tiempo de Sincronizacion: %d", attr->sync_time);
 
+    fs_blocks_manager_init(attr->mount_point);
+    fs_bitacoras_manager_init(attr->mount_point);
+    fs_files_manager_init(attr->mount_point);
     fs_sabotage_notifier_init(attr->sabotage_positions);
 }
 
@@ -31,6 +55,7 @@ void  file_system_inicio_tarea(uint32_t tid, const char* tarea)
         string_from_format("Tripulante %d inicia tarea %s", tid, tarea);
 
     file_system_update_bitacora(tid, inicio_tarea);
+    file_system_execute_task(tarea);
 }
 
 void  file_system_finalizacion_tarea(uint32_t tid, const char* tarea)
@@ -68,25 +93,78 @@ char* file_system_obtener_bitacora(uint32_t tid)
 //             *** Private Functions ***
 // ========================================================
 
-private void  file_system_create_bitacora(uint32_t tid)
-{
-    U_LOG_TRACE("Se crea bitacora para tripulante %d", tid);
-}
-
-private void  file_system_remove_bitacora(uint32_t tid)
-{
-    U_LOG_TRACE("Se elimina bitacora para tripulante %d", tid);
-}
-
 private void  file_system_update_bitacora(uint32_t tid, const char* content)
 {
-    // TODO: inspeccionar en el fs que exista la bitacora del tripulante
-    U_LOG_TRACE("Se actualiza bitacora de %d", tid);
-    U_LOG_TRACE("Contenido: %s", content);
+    fs_bitacora_t* bitacora = fs_bitacoras_manager_hold_bitacora(tid);
+
+    if(bitacora == NULL)
+        fs_bitacoras_manager_create_bitacora(tid);
+
+    bitacora = fs_bitacoras_manager_hold_bitacora(tid);
+
+    fs_bitacora_add_content(bitacora, content);
+    fs_bitacoras_manager_release_bitacora(tid);
 }
 
 private char* file_system_get_bitacora(uint32_t tid)
 {
     U_LOG_TRACE("Se pide bitacora de tripulante %d", tid);
-    return string_from_format("Bitacora Tripulante %d", tid);
+
+    fs_bitacora_t* bitacora = fs_bitacoras_manager_hold_bitacora(tid);
+
+    if(bitacora == NULL)
+        return NULL;
+
+    char* content = fs_bitacora_get_content(bitacora);
+    fs_bitacoras_manager_release_bitacora(tid);
+
+    return content;
+}
+
+private void file_system_execute_task(const char* task)
+{
+    fs_task_parser_result_t* task_instance = fs_task_parser_parse(task);
+
+    if(task_instance == NULL)
+        U_LOG_ERROR("No se pudo parsear la tarea recibida: %s", task);
+    else
+    {
+        if(task_instance->type != FS_TASK_UNKNOWN)
+            TASK_EXEC_FUNCS[task_instance->type](task_instance);
+
+        fs_task_parser_result_delete(task_instance);
+    }
+}
+
+private void file_system_add_char_fill(const fs_task_parser_result_t* task)
+{
+    fs_file_t* file = fs_files_manager_hold_file(task->file_name);
+    
+    if(file == NULL)
+    {
+        fs_files_manager_create_file(task->file_name, task->file_name[0]);
+        file = fs_files_manager_hold_file(task->file_name);
+    }
+
+    fs_file_add_fill_char(file, task->arg);
+    fs_files_manager_release_file(task->file_name);
+}
+
+private void file_system_rm_char_fill(const fs_task_parser_result_t* task)
+{
+    fs_file_t* file = fs_files_manager_hold_file(task->file_name);
+    
+    if(file == NULL)
+    {
+        fs_files_manager_create_file(task->file_name, task->file_name[0]);
+        file = fs_files_manager_hold_file(task->file_name);
+    }
+
+    fs_file_remove_fill_char(file, task->arg);
+    fs_files_manager_release_file(task->file_name);
+}
+
+private void file_system_rm_file(const fs_task_parser_result_t* task)
+{
+    fs_files_manager_delete_file(task->file_name);
 }
