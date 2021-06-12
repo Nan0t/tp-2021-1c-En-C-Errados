4aaa67956187674fcedff87b6e03feac4ea55d5d
cmessuti <claudio.messuti@gmail.com>
2021-06-12 00:10:45 -0300

    Se agrega codificacion de las primeras interfaces de segmentacion

--- Mi-RAM-HQ/src/memoria/esquemas/segmentacion/segmentacion.h
+++ Mi-RAM-HQ/src/memoria/esquemas/segmentacion/segmentacion.h
@@ -3,18 +3,53 @@
 
 #include "memoria/esquemas/esquema_memoria.h"
 
+typedef struct
+{
+    uint32_t inicio_segmento;
+    uint32_t fin_segmento;
+    uint32_t tipo_segmento;
+    uint32_t id_propietario;
+}p_segmento_t;
+
+typedef struct
+{
+    uint32_t pid;
+    t_list* tabla_segmentos;
+}p_patota_y_tabla_t;
+
+typedef struct
+{
+	uint32_t inicio_segmento_pcb;
+    uint32_t tamanio_segmento_pcb;
+    uint32_t inicio_segmento_tareas;
+    uint32_t tamanio_segmento_tareas;
+	t_list*  listado_tripulantes;
+}p_segmento_patota_t;
+
+typedef struct
+{
+	uint32_t tid;
+	uint32_t inicio_segmento_tcb;
+    uint32_t tamanio_segmento_tcb;
+}p_segmento_tripulante_t;
+
+t_list* listado_segmentos;
+
 void segmentacion_memoria_init(void);
 
 bool segmentacion_memoria_inicializar_patota(uint32_t pid, uint32_t cant_tripulantes, const char* tareas);
-void segmentacion_memoria_inicializar_tripulante(uint32_t pid, uint32_t tid, u_pos_t pos);
+bool segmentacion_memoria_inicializar_tripulante(uint32_t pid, uint32_t tid, u_pos_t pos);
 
-void segmentacion_memoria_actualizar_posicion_tripulante(uint32_t pid, uint32_t tid, u_pos_t pos);
-void segmentacion_memoria_actualizar_estado_tripulante(uint32_t pid, uint32_t tid, char estado);
+bool segmentacion_memoria_actualizar_posicion_tripulante(uint32_t pid, uint32_t tid, u_pos_t pos);
+bool segmentacion_memoria_actualizar_estado_tripulante(uint32_t pid, uint32_t tid, char estado);
 
 char*          segmentacion_memoria_obtener_proxima_tarea(uint32_t pid, uint32_t tid);
 tripulantes_t* segmentacion_memoria_obtener_info_tripulante(uint32_t pid, uint32_t tid);
 t_list*        segmentacion_memoria_obtener_todos_los_tripulantes(void);
 
-void           segmentacion_memoria_expulsar_tripulante(uint32_t pid, uint32_t tid);
+bool           segmentacion_memoria_expulsar_tripulante(uint32_t pid, uint32_t tid);
+
+//-----------------------
+
 
 #endif
