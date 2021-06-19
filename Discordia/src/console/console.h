ffe84e5bd538b8375326287dd4662b352bce676a
int0x80-sys <masosa@frba.utn.edu.ar>
2021-06-19 17:43:19 +0000

    [doc]: se agrega documentacion a las funciones de las interfaces de la consola. Se elimina el codigo de debug (reside actualmente en la rama debug-console)

--- Discordia/src/console/console.h
+++ Discordia/src/console/console.h
@@ -1,6 +1,10 @@
 #ifndef DISCORDIA_CONSOLE_H
 #define DISCORDIA_CONSOLE_H
 
+/**
+ * @NAME: ds_console_init
+ * @DESC: inicializa la consola del Discordiador.
+ */
 void ds_console_init(void);
 
 #endif
